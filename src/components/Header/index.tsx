611606c678c7e75152007da2f56bfd801b74352c
Henrique França <euhenriquefranca@gmail.com>
2021-08-03 11:36:36 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,24 @@
+import { Flex, Img } from '@chakra-ui/react';
+import Link from 'next/link';
 
-export function Home() {
-  return(
-    <h1>Header</h1>
-  )
+export function Header() {
+  return (
+    <Flex
+      w="100%"
+      as="header"
+      align="center"
+      justify="center"
+      my={['4', '5', '6']}
+    >
+      <Link href="/">
+        <a>
+          <Img
+            w={['5.06rem', '8rem', '11.56rem']}
+            src="assets/logo.png"
+            alt="Logo"
+          />
+        </a>
+      </Link>
+    </Flex>
+  );
 }
